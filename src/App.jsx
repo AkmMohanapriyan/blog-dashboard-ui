ddb3a4db35de3447dfac0f8ef2103833f585eab6
AkmMohanapriyan <mohanapriyanpriyan4@gmail.com>
2025-05-01 17:51:50 +0530

    Re Write Route path with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './Pages/Login'
-import UserDashBoard from './Pages/UserDashBoard'
-import AdminDashboard from "../src/Pages/AdminDashBoard"
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import AdminDashboard from "../src/Pages/AdminDashBoard";
+import UserDashboard from "../src/Pages/UserDashBoard";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
 
   return (
-    <>
 
-<Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/user-dashboard" element={<UserDashBoard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <HashRouter>
+      {/* <Router> */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        </Routes>
+      {/* </Router> */}
+      </HashRouter>
 
 
-    </>
   )
 }
 
